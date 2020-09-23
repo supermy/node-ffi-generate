76252d9747d1e6305f541adb4bc143bb9a18c168
Joel Purra <mig@joelpurra.se>
2020-09-23 14:05:27 +0200

    Add simple struct tests

---
+++ test/struct/empty.js
@@ -0,0 +1,33 @@
+const test = require("ava");
+const fs = require("fs");
+const {
+	promisify,
+} = require("util");
+
+const assertExpectedLines = require("../helper/assert-expected-lines");
+
+const writeFile = promisify(fs.writeFile);
+
+test("lines", async (t) => {
+	const {
+		generate,
+	} = require("../..");
+
+	const generated = await generate({
+		filepath: `${__filename}.h`,
+		library: "simple",
+	});
+
+	await writeFile(__filename + ".output.js", generated.serialized);
+
+	t.is(generated.unmapped.length, 0);
+
+	const expectedTypes = `const my_struct_t = voidPtr;
+	const my_struct_tPtr = ref.refType(my_struct_t);`;
+
+	assertExpectedLines(t, expectedTypes, generated.serialized);
+
+	const expectedFunctions = "do_stuff: [ref.types.void, [my_struct_t]],";
+
+	assertExpectedLines(t, expectedFunctions, generated.serialized);
+});
---
+++ test/struct/simple.js.h
@@ -0,0 +1,6 @@
+typedef struct my_struct
+{
+  void *my_void;
+} my_struct_t;
+
+void do_stuff(my_struct_t *stuff);
