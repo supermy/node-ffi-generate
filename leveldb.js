d4c32da89a21d50bf260d83f41d3e38f5d6b4110
supermy <springclick@gmail.com>
2021-09-23 11:27:19 +0800

    增加 leveldb.js NodeJS 调用 leveldb

---
+++ leveldb.js
@@ -0,0 +1,20 @@
+// $ npm install levelup leveldown
+
+var levelup = require('levelup')
+var leveldown = require('leveldown')
+
+// 1) Create our store
+var db = levelup(leveldown('./db/leveldb'))
+
+// 2) Put a key & value
+db.put('name', 'levelup', function (err) {
+	if (err) return console.log('Ooops!', err) // some kind of I/O error
+
+	// 3) Fetch by key
+	db.get('name', function (err, value) {
+		if (err) return console.log('Ooops!', err) // likely the key was not found
+
+		// Ta da!
+		console.log('name=' + value)
+	})
+})
